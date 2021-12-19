b9a09717a6d72be7fcb9b4d1c3660a7f85703fc5
Sarah <sarah.eddeb@mail.utoronto.ca>
2021-12-19 01:01:45 -0500

    Fix heroku error

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const app = express()
 app.use(express.static(__dirname + '/pub'))
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + '/pub/gridsLanding.html'));
+    res.sendFile(path.resolve(__dirname, "build", '/pub/gridsLanding.html'));
+
+    // res.sendFile(path.join(__dirname + '/pub/gridsLanding.html'));
+    // res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
+    // res.sendFile(path.join(__dirname + '/pub/grids.html'));
+
 })
 
 app.listen(process.env.PORT || 9000, 
