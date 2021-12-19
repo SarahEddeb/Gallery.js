ea3c7172d28261e6c27c8444c9a6d8d9d3e565de
Sarah <sarah.eddeb@mail.utoronto.ca>
2021-12-19 02:36:15 -0500

    Trying to get this to work

--- server.js
+++ server.js
@@ -9,30 +9,26 @@ const app = express()
 app.use(express.static(__dirname + '/pub'))
 
 app.get("/", function (req, res) {
-    // res.sendFile(path.resolve(__dirname, "build", '/pub/gridsLanding.html'));
-
-    res.sendFile(path.join(__dirname + '/pub/gridsLanding.html'));
-    // res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
-    // res.sendFile(path.join(__dirname + '/pub/grids.html'));
-})
-
-app.get("/pub/gridsLanding", function (req, res) {
-    // res.sendFile(path.resolve(__dirname, "build", '/pub/gridsLanding.html'))
     res.sendFile(path.join(__dirname + '/pub/gridsLanding.html'));
 })
 
-app.get("/pub/gridsExample", function (req, res) {
+// app.get("/pub/gridsLanding", function (req, res) {
+//     // res.sendFile(path.resolve(__dirname, "build", '/pub/gridsLanding.html'))
+//     res.sendFile(path.join(__dirname + '/pub/gridsLanding.html'));
+// })
 
-    res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
-    // res.sendFile(path.join(__dirname + '/pub/gridsExamp
-})
-app.get("/pub/gridsAPI", function (req, res) {
-    // res.sendFile(path.resolve(__dirname, "build", '/pub/gridsLanding.html'));
+// app.get("/pub/gridsExample", function (req, res) {
 
-    // res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
-    // // res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
-    res.sendFile(path.join(__dirname + '/pub/gridsAPI.html'));
-})
+//     res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
+//     // res.sendFile(path.join(__dirname + '/pub/gridsExamp
+// })
+// app.get("/pub/gridsAPI", function (req, res) {
+//     // res.sendFile(path.resolve(__dirname, "build", '/pub/gridsLanding.html'));
+
+//     // res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
+//     // // res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
+//     res.sendFile(path.join(__dirname + '/pub/gridsAPI.html'));
+// })
 
 app.listen(process.env.PORT || 9000, 
 	() => log("Server is running on port 9000..."));
