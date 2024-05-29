3bcc04214f4e853770ff6f5526ef02590d55945e
SarahEddeb <sarah.ed123@gmail.com>
2024-05-29 13:29:27 -0400

    Remove commented out code

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Grids
-<<<<<<< HEAD
 Javascript library that allows you, the developer, to create interesting interactive photo grid elements.
-=======
-Javascript library that allows you, the developer, to create interesting interactive photo grid elements.
->>>>>>> 766fc8c7678fd060aec5f71b23c7574ee1bc44a4
--- server.js
+++ server.js
@@ -12,23 +12,5 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + '/pub/gridsLanding.html'));
 })
 
-// app.get("/pub/gridsLanding", function (req, res) {
-//     // res.sendFile(path.resolve(__dirname, "build", '/pub/gridsLanding.html'))
-//     res.sendFile(path.join(__dirname + '/pub/gridsLanding.html'));
-// })
-
-// app.get("/pub/gridsExample", function (req, res) {
-
-//     res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
-//     // res.sendFile(path.join(__dirname + '/pub/gridsExamp
-// })
-// app.get("/pub/gridsAPI", function (req, res) {
-//     // res.sendFile(path.resolve(__dirname, "build", '/pub/gridsLanding.html'));
-
-//     // res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
-//     // // res.sendFile(path.join(__dirname + '/pub/gridsExample.html'));
-//     res.sendFile(path.join(__dirname + '/pub/gridsAPI.html'));
-// })
-
 app.listen(process.env.PORT || 9001, 
 	() => log("Server is running on port 9001..."));
