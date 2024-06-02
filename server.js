709eaa97cd0ba508d5e561179dc50a5ccdf58a78
SarahEddeb <sarah.ed123@gmail.com>
2024-06-02 16:04:47 -0400

    Update server.js with correct file paths

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.static(__dirname + '/pub'))
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + '/pub/gridsLanding.html'));
+    res.sendFile(path.join(__dirname + '/pub/Landing.html'));
 })
 
 app.listen(process.env.PORT || 9001, 
