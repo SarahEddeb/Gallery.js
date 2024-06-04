81385f0ddfb46663b42c53780a17b89df8ad1053
SarahEddeb <sarah.ed123@gmail.com>
2024-06-04 00:20:33 -0400

    Add basic row to landing page

--- gallery.js/src/pages/home.js
+++ gallery.js/src/pages/home.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
+import Row from "photo-gallery.js";
 
 const PrimaryButton = styled.button`
   align-items: center;
@@ -73,7 +74,43 @@ const HeroSection = styled.div`
   margin-top: 5vh;
 `;
 
+const RowContainer = styled.div`
+  width: 100%;
+`;
+
 function Home() {
+  const rowRef = useRef(null);
+
+  //   function MakeRow() {
+  useEffect(() => {
+    const row = new Row(7, "30vh");
+
+    row.addBackgroundImg(
+      0,
+      "https://images.unsplash.com/photo-1517315403381-a12569a9da85?q=80&w=3280&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    );
+    row.addBackgroundImg(
+      1,
+      "https://images.unsplash.com/photo-1517314815091-85f25384be22?q=80&w=3280&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    );
+    row.addHoverAnimation("height");
+    row.changeAligment("bottom");
+    row.changeBorderRadius(0);
+    row.changeXMargins(4);
+    //   return row;
+
+    if (rowRef.current) {
+      rowRef.current.innerHTML = ""; // Clear previous content
+      rowRef.current.appendChild(row.row);
+    }
+
+    return () => {
+      if (rowRef.current && row.row) {
+        rowRef.current.removeChild(row.row);
+      }
+    };
+  }, []);
+  //   }
   return (
     <div>
       <HeroSection>
@@ -91,6 +128,7 @@ function Home() {
         </ButtonGroup>
       </HeroSection>
 
+      <RowContainer ref={rowRef} />
     </div>
   );
 }
