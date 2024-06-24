05813dea5e720d4b2bcdead2aae2f69c0771a2fe
SarahEddeb <sarah.ed123@gmail.com>
2024-06-24 00:42:20 -0400

    Fix basename for BrowserRouter

--- gallery.js/src/App.js
+++ gallery.js/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home.js";
+import Home from "./pages/Home.js";
 import Documentation from "./pages/Documentation.js";
 
 function App() {
   return (
-    <BrowserRouter basename="/Photo-Gallery.js">
+    <BrowserRouter basename="/Gallery.js">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="documentation" element={<Documentation />} />
